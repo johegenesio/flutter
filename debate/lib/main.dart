30739543e3558092d5a2d27ca847899671530cc2
João Henrique Martins Genesio <110195453+johegenesio@users.noreply.github.com>
2024-05-12 16:57:13 -0300

    .

--- debate/lib/main.dart
+++ debate/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -17,8 +16,8 @@ class _MyAppState extends State<MyApp> {
   int paginaAtual = 0;
   List paginas = [
     const Home(),
-    const Text(''),
     const Text('CARRINHO'),
+    const Text('sa'),
     const Text('oi'),
   ];
 
@@ -34,31 +33,70 @@ class _MyAppState extends State<MyApp> {
         debugShowCheckedModeBanner: false,
         title: 'Debate',
         theme: ThemeData(
-          primaryColor: Colors.blue[400],
           textTheme: GoogleFonts.poppinsTextTheme(),
           useMaterial3: true,
         ),
         home: Scaffold(
             appBar: AppBar(
+              backgroundColor: const Color.fromARGB(255, 212, 212, 212),
               title: const Text(
-                're.star',
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+                'E-commerce',
+                style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 14,
+                    color: Colors.black87),
               ),
-              actions: const [Icon(Icons.search)],
+              actions: const [
+                Icon(
+                  Icons.search,
+                  color: Colors.black87,
+                ),
+                SizedBox(
+                  width: 16,
+                )
+              ],
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(4),
+                ),
+              ),
               centerTitle: true,
               elevation: 2,
             ),
             body: paginas[paginaAtual],
             bottomNavigationBar: NavigationBar(
+              indicatorColor: Colors.grey[200],
+              backgroundColor: const Color.fromARGB(255, 212, 212, 212),
+              labelBehavior:
+                  NavigationDestinationLabelBehavior.onlyShowSelected,
               selectedIndex: paginaAtual,
               onDestinationSelected: proximaPagina,
               destinations: const [
                 NavigationDestination(
-                    icon: Icon(Icons.home_filled), label: 'Inicio'),
+                  icon: Icon(
+                    Icons.home_filled,
+                    color: Colors.black87,
+                  ),
+                  label: 'Inicio',
+                ),
                 NavigationDestination(
-                    icon: Icon(Icons.shopping_cart_rounded), label: 'Carrinho'),
-                NavigationDestination(icon: Icon(Icons.menu), label: 'Menu'),
-                NavigationDestination(icon: Icon(Icons.person), label: 'Conta')
+                    icon: Icon(
+                      Icons.shopping_cart,
+                      color: Colors.black87,
+                    ),
+                    label: 'Carrinho'),
+                NavigationDestination(
+                    icon: Icon(
+                      Icons.menu,
+                      color: Colors.black87,
+                    ),
+                    label: 'Menu'),
+                NavigationDestination(
+                    icon: Icon(
+                      Icons.person,
+                      color: Colors.black87,
+                    ),
+                    label: 'Conta'),
               ],
             )));
   }
@@ -70,6 +108,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: ListView(
         children: [
           Padding(
@@ -78,56 +117,276 @@ class Home extends StatelessWidget {
                 width: double.infinity,
                 height: 200,
                 decoration: BoxDecoration(
-                  color: Colors.black,
+                  color: const Color.fromRGBO(70, 70, 70, 1),
                   borderRadius: BorderRadius.circular(8),
                 )),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.fromLTRB(8, 0, 8, 4),
             child: Container(
               width: double.infinity,
-              height: 80,
+              height: 40,
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8), color: Colors.black),
+                  borderRadius: BorderRadius.circular(8),
+                  color: const Color.fromRGBO(70, 70, 70, 1)),
             ),
           ),
           SizedBox(
             width: double.infinity,
-            height: 200,
+            height: 112,
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
               child: ListView(
                 scrollDirection: Axis.horizontal,
-                children: [
-                  Column(
-                    children: [CircleAvatar(), Text('Celular')],
-                  ),
-                  Column(
-                    children: [CircleAvatar(), Text('TVs')],
-                  ),
-                  Column(
-                    children: [CircleAvatar(), Text('Informática')],
-                  ),
-                  Column(
-                    children: [CircleAvatar(), Text('Esporte')],
-                  ),
+                children: const [
+                  Categoria(imageAssets: '/celular.jpg', textLabel: 'Celular'),
+                  Categoria(imageAssets: '/celular.jpg', textLabel: "TV's"),
+                  Categoria(
+                      imageAssets: '/celular.jpg', textLabel: 'Informática'),
+                  Categoria(imageAssets: '/celular.jpg', textLabel: 'Moda'),
+                  Categoria(imageAssets: '/celular.jpg', textLabel: 'Esporte'),
+                  Categoria(imageAssets: '/celular.jpg', textLabel: 'Lazer'),
+                  Categoria(imageAssets: '/celular.jpg', textLabel: 'Lar'),
+                  Categoria(imageAssets: '/celular.jpg', textLabel: 'Eletros'),
+                  Categoria(imageAssets: '/celular.jpg', textLabel: 'Animais'),
+                  Categoria(imageAssets: '/celular.jpg', textLabel: 'Moda'),
                 ],
               ),
             ),
           ),
-          Wrap(
-            children: [
-              Column(
-                children: [
-                  Image.asset(''),
-                  Text(''),
-                  Text('')
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+            child: Divider(
+              color: Colors.black26,
+              thickness: 0.2,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(8, 8, 0, 4),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(Icons.favorite),
+                SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  'Para você',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 300,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
                 ],
-              )
-            ],
-          )
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+            child: Divider(
+              color: Colors.black26,
+              thickness: 0.2,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(8, 4, 0, 4),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(Icons.call_made),
+                SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  'Em alta',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 300,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: const [
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
+                  Produtos(
+                      nomeDoProduto:
+                          'Notebook Predator Helios Neo PHN16-71-76PL Ci7 13° Windows 11 Home 16GB 512GB SSD RTX 4050 16” WUXGA',
+                      preco: '999',
+                      imageProduto: '/note.jpg'),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class Categoria extends StatelessWidget {
+  const Categoria(
+      {super.key, required this.imageAssets, required this.textLabel});
+
+  final String imageAssets;
+  final String textLabel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+      child: SizedBox(
+        width: 64,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 2, color: Colors.black12, spreadRadius: 1)
+                ],
+              ),
+              child: CircleAvatar(
+                radius: 32,
+                backgroundImage: AssetImage(imageAssets),
+              ),
+            ),
+            Text(
+              textLabel,
+              style: const TextStyle(fontSize: 10, fontWeight: FontWeight.w100),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Produtos extends StatelessWidget {
+  const Produtos(
+      {super.key,
+      required this.nomeDoProduto,
+      required this.preco,
+      required this.imageProduto});
+
+  final String nomeDoProduto;
+  final String preco;
+  final String imageProduto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.white,
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black12,
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                )
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    imageProduto,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4.0),
+                  child: Text(
+                    nomeDoProduto,
+                    maxLines: 2,
+                    style: const TextStyle(fontSize: 12, height: 1.2),
+                  ),
+                ),
+                Text(
+                  'R\$$preco',
+                  style: const TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.w700),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
